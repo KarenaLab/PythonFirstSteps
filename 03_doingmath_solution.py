108eb4aa614a0143aacfe1039210256d41a4dad2
Seb Karena <EKChikui@gmail.com>
2025-08-10 11:32:21 -0300

    Create 03_doingmath_solution.py

---
+++ 03_doingmath_solution.py
@@ -0,0 +1,42 @@
+
+# Calcule 1+1 e mostre o resultado
+x = 1 + 1
+print(x)
+print("")
+
+
+# Calcule 60 - 3
+print("A solução da equação 60 - 3 é")
+x = 60 - 3
+print(x)
+print("")
+
+
+# Calcule 3 x 5
+print("3 x 5 = ", end="")
+print(3 * 5, end="\n\n")
+
+
+# Calcule a expressão 1500 + (1500 x 5 / 100)
+x = (1500 + ((1500 * 5) / 100))
+print(x)
+
+
+# Calcule 2^3 (Lembre-se do Excel, "2 elevado ao cubo")
+numero = 2
+exponente = 3
+resultado = numero ** exponente
+print(resultado)
+
+
+# Calcule a expressão 425,24 * (1 + (13,87 / 100)) ^ 29
+pf28 = (735.35 * (1 + (1.1 / 100)) ** 29)
+print(pf28)
+
+
+# Informe o resto da divisão de 16 por 3
+x = 16 % 3
+print(f"O resto da divisão 16 por 3 é {x}")
+
+
+# Calcule a raiz quadrada de 16
