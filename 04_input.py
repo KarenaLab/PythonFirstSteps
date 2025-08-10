581f365fdfd8efd2e1ebe3a62a111bf2199963fa
Seb Karena <EKChikui@gmail.com>
2025-08-10 12:05:39 -0300

    Update 04_input.py

--- 04_input.py
+++ 04_input.py
@@ -1,3 +1,5 @@
 
-x = input("Digite o valor do salário: ")
-print(x)
+salario = input("Digite o valor do salário: ")
+aumento = input("Digite o percentual do aumento: ")
+
+novo_salario = salario * (1 + (aumento / 100))
